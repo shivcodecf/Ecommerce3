70c7efa7a652916dcb8bd034fae9187ff041d215
shivcodecf <shivam2113128@akgec.ac.in>
2025-01-17 01:21:11 +0530

    Describe your changes here

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -25,7 +25,7 @@ function Login() {
 
     try {
       const response = await axios.post(
-        'http://localhost:5000/login',
+        'https://ecomm-back2.onrender.com/login',
         { email, password },
         {
           headers: { 'Content-Type': 'application/json' },
